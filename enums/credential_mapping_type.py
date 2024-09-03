cec817883b4bc8a4b952028d6095de02bf1f3f33
Akiff Manji <akiff.manji@quartech.com>
2024-09-03 15:44:48 +0000

    refactor: expand mapping types
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- enums/credential_mapping_type.py
+++ enums/credential_mapping_type.py
@@ -5,4 +5,5 @@ class CredentialMappingTypeEnum(str, Enum):
     """CredentialMappingAttribute enum"""
 
     EFFECTIVE_DATE = "effective_date"
+    EXPIRY_DATE = "expiry_date"
     REVOKED_DATE = "revoked_date"
