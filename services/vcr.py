4f0e0e84d5762833ef2d3797864a65c163a68072
Akiff Manji <akiff.manji@quartech.com>
2024-09-11 14:23:03 +0000

    feat: add pydantic settings
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- services/vcr.py
+++ services/vcr.py
@@ -2,24 +2,27 @@
 
 from httpx import AsyncClient
 
+from config import settings
+
 
 async def register_credential_type(credential_type):
     """Register a new credential type"""
 
     # Make an HTTP request to the Aries VCR API
     async with AsyncClient(
-        base_url="http://host.docker.internal:8008/agentcb/topic"
+        base_url=f"{settings.aries_vcr_url}/agentcb/topic"
     ) as client:
         response = await client.post("/vc_di_credential_type/", json=credential_type)
         response.raise_for_status()
         return response.json()
+
 
 async def issue_credential(credential):
     """Issue a new credential"""
 
     # Make an HTTP request to the Aries VCR API
     async with AsyncClient(
-        base_url="http://host.docker.internal:8008/agentcb/topic"
+        base_url=f"{settings.aries_vcr_url}/agentcb/topic"
     ) as client:
         response = await client.post("/vc_di_credential/", json=credential)
         response.raise_for_status()
