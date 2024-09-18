9dcfe619bddbf9c4c37602dd6e13bc939d4f7a14
Patrick <pat.stlouis91@gmail.com>
2024-09-18 02:44:30 +0000

    add temporary issuer registry in configuration file
    Signed-off-by: Patrick <pat.stlouis91@gmail.com>
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- config.py
+++ config.py
@@ -4,5 +4,16 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     aries_vcr_url: str = "http://host.docker.internal:8080"
 
+    ISSUERS: list = [
+        {
+            "id": "did:web:test.digitaltrust.traceability.site:petroleum-and-natural-gas-act:director-of-petroleum-lands",
+            "name": "Director of Petroleum Lands",
+        },
+        {
+            "id": "did:web:test.digitaltrust.traceability.site:mines-act:chief-permitting-officer",
+            "name": "Chief Permitting Officer",
+        },
+    ]
+
 
 settings = Settings()
