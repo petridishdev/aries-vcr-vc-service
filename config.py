3e65032ef47e4919749633ed10e41b5575004be2
PatStLouis <patrick.st-louis@opsecid.ca>
2024-09-20 02:05:10 +0000

    add app title
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    PROJECT_TITLE: str = "Aries VCR VC Service"
+    
     aries_vcr_url: str = "http://host.docker.internal:8080"
 
     ISSUERS: list = [
