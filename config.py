bb61dc62e62069471292d1639f6992239418820c
Akiff Manji <akiff.manji@quartech.com>
2024-09-21 22:54:32 +0000

    chore: add types
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     aries_vcr_url: str = "http://host.docker.internal:8080"
 
-    ISSUERS: list = [
+    issuers: list[dict] = [
         {
             "id": "did:web:test.digitaltrust.traceability.site:petroleum-and-natural-gas-act:director-of-petroleum-lands",
             "name": "Director of Petroleum Lands",
