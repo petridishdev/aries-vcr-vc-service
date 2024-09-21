61c8046fb5411a31bd344bd7894a0f42d09e7904
Akiff Manji <akiff.manji@quartech.com>
2024-09-21 22:55:27 +0000

    feat: add credential get routes
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- services/vcr.py
+++ services/vcr.py
@@ -5,7 +5,7 @@ from httpx import AsyncClient
 from config import settings
 
 
-async def register_credential_type(credential_type):
+async def register_credential_type(credential_type: dict):
     """Register a new credential type"""
 
     # Make an HTTP request to the Aries VCR API
@@ -17,7 +17,7 @@ async def register_credential_type(credential_type):
         return response.json()
 
 
-async def issue_credential(credential):
+async def issue_credential(credential: dict):
     """Issue a new credential"""
 
     # Make an HTTP request to the Aries VCR API
@@ -25,5 +25,18 @@ async def issue_credential(credential):
         base_url=f"{settings.aries_vcr_url}/agentcb/topic"
     ) as client:
         response = await client.post("/vc_di_credential/", json=credential)
+        response.raise_for_status()
+        return response.json()
+
+
+async def get_credential(credential_id: str):
+    """Get a credential from its UUID"""
+
+    # Make an HTTP request to the Aries VCR API
+    async with AsyncClient(base_url=f"{settings.aries_vcr_url}/api") as client:
+        response = await client.get(
+            f"/credential/{credential_id}",
+            params={"raw_data": "true"},
+        )
         response.raise_for_status()
         return response.json()
