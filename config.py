1b8e034106fe723d476222e354a002c89e555b41
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-21 15:16:00 +0000

    add vcr url as env var
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv(os.path.join(basedir, ".env"))
 class Settings(BaseSettings):
     project_title: str = "Aries VCR VC Service"
 
-    aries_vcr_url: str = "http://host.docker.internal:8080"
+    aries_vcr_url: str = os.environ["ARIES_VCR_URL"]
 
     issuer_registry_url: str = os.environ["ISSUER_REGISTRY_URL"]
 
